bf119f8b1029b78f8c7b400d78029607d19b1382
Minji Song <101618759+devchosil@users.noreply.github.com>
2022-07-17 12:08:39 +0900

    Create users.js

---
+++ users.js
@@ -0,0 +1,198 @@
+[
+    {
+      "id" : 1,
+      "userId" : "my_2022",
+      "userName" : "yoona",
+      "intro" : "I'm front-end developer",
+      "profile" : "profilePic/profile.jpg",
+      "pic" : "images/img.jpg",
+      "overlap" : "benedict님 외 3명이 팔로우합니다",
+      "likeCount" : 5,
+      "content" : "오랜만에 친구들이랑 브런치💖 웨이팅은 좀 오래했지만 사람이 괜히 많은게 아니네👍 다음에 또 와야징!! 역시 주말엔 좋아하는 사람들과 수다 떨면서 맛있는거 먹는게 최고! ",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "heheheh",
+          "comment" : "모야 넘 맛있겠다 여기 어디야?!"
+        },{
+          "id" : 2,
+          "유저id" : "chuibbo",
+          "comment" : "나랑도 놀쟈"
+        },{
+          "id" : 3,
+          "유저id" : "jusiktooja",
+          "comment" : "맛잇겠구먼"
+        } ]
+    },
+    {
+      "id" : 2,
+      "userId" : "love_puppy",
+      "userName" : "David",
+      "intro" : "I have 2 dogs. schnauzer and chihuahua❤",
+      "profile" : "profilePic/profile1.jpg",
+      "pic" : "images/img1.jpg",
+      "overlap" : "hyevley님 외 1명이 팔로우합니다",
+      "likeCount" : 2,
+      "content" : "냠냠",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "gkglaemfek",
+          "comment" : "다이어트 한다더니"
+        },{
+          "id" : 2,
+          "유저id" : "huangung",
+          "comment" : "직접 한거야? 대단하넹"
+        }]
+    },
+    {
+      "id" : 3,
+      "userId" : "benedict",
+      "userName" : "David",
+      "intro" : "-",
+      "profile" : "profilePic/profile2.jpg",
+      "pic" : "images/img2.jpg",
+      "overlap" : "nyamnyam님 외 4명이 팔로우합니다",
+      "likeCount" : 1,
+      "content" : "매일 가고 싶은 카페! 오늘은 레몬케이크 먹으면서 혼자 과제함ㅋㅋㅋ ",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "boksil2",
+          "comment" : "과제 다해써?!!"
+        },{
+          "id" : 2,
+          "유저id" : "mimimi",
+          "comment" : "요기 잠실에 있는거 맞지?! 카페도 예쁘고 커피도 마싰더라"
+        },  {
+          "id" : 3,
+          "유저id" : "huangung",
+          "comment" : "나랑두가"
+        }]
+    },
+    {
+      "id" : 4,
+      "userId" : "macaron",
+      "userName" : "David",
+      "intro" : "마카롱 러버",
+      "profile" : "profilePic/profile3.jpg",
+      "pic" : "images/img3.jpg",
+      "overlap" : "huhuhuh님 외 2명이 팔로우합니다",
+      "likeCount" : 2,
+      "content" : "역시 제주도 최고👍 사진 보니까 다시 돌아가고 싶다ㅠㅠ 다음에도 시간되면 바로 날아오겠어!! 요앞에서 먹은 김밥도 존맛",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "hotta",
+          "comment" : "헐 제주도 언제 갔다왔어 좋았겠다"
+        },{
+          "id" : 2,
+          "유저id" : "yerin_bak",
+          "comment" : "오"
+        }]
+    },
+    {
+      "id" : 5,
+      "userId" : "vegan",
+      "userName" : "David",
+      "intro" : "I don't eat animal products",
+      "profile" : "profilePic/profile4.jpg",
+      "pic" : "images/img4.jpg",
+      "overlap" : "duqhtpdy님 외 1명이 팔로우합니다",
+      "likeCount" : 10,
+      "content" : "하늘색이 예뻐서 찍은 사진",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "haruilki",
+          "comment" : "오 어디냐"
+        }]
+    },
+    {
+      "id" : 6,
+      "userId" : "hyevley",
+      "userName" : "David",
+      "intro" : "hyevley's blog / live in KR",
+      "profile" : "profilePic/profile5.jpg",
+      "pic" : "images/img5.jpg",
+      "overlap" : "yunE2님 외 1명이 팔로우합니다",
+      "likeCount" : 0,
+      "content" : "일끝나고 호텔에서 먹는 간식! 낼 새벽 일찍 일어나서 다시 일가야하지만 ㅠㅠㅠ 잠깐이라도 맛난거 먹으면서 머리식힐래",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "yunE2",
+          "comment" : "아이고 수고했어"
+        },{
+          "id" : 2,
+          "유저id" : "peperoooo",
+          "comment" : "나 저 우동 완전 조아하는데!!!!! "
+        }]
+    },
+    {
+      "id" : 7,
+      "userId" : "kxxxjxhxxx",
+      "userName" : "David",
+      "intro" : "hyevley's blog / live in KR",
+      "profile" : "profilePic/profile6.jpg",
+      "pic" : "images/img6.jpg",
+      "overlap" : "huangung님 외 1명이 팔로우합니다",
+      "likeCount" : 3,
+      "content" : "ㅋㅋㅋㅋㅋㅋ 애기 장난감 넘 귀여워 친구네집 갔다가 친구아들 장난감 내가 계속 갖고놈",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "kamomile",
+          "comment" : "ㅋㅋㅋㅋㅋ애기 장난감 뺏지마라"
+        },{
+          "id" : 2,
+          "유저id" : "sysysy_",
+          "comment" : "쏘큐트ㅋㅋㅋ"
+        }]
+    },
+    {
+      "id" : 8,
+      "userId" : "hyevley",
+      "userName" : "David",
+      "intro" : "hyevley's blog / live in KR",
+      "profile" : "profilePic/profile7.jpg",
+      "pic" : "images/img7.jpg",
+      "overlap" : "heheheh님 외 1명이 팔로우합니다",
+      "likeCount" : 8,
+      "content" : "크리스마스 준비💖 미니 크리스마스 트리",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "kamomile",
+          "comment" : "귀엽당ㅋㅋㅋ 벌써부터 크리스마스 준비하는거냐"
+        },{
+          "id" : 2,
+          "유저id" : "huangung",
+          "comment" : "ㅋㅋ"
+        }]
+    },
+    {
+      "id" : 9,
+      "userId" : "rostain",
+      "userName" : "David",
+      "intro" : "hyevley's blog / live in KR",
+      "profile" : "profilePic/profile8.jpg",
+      "pic" : "images/img8.jpg",
+      "overlap" : "sysysy_님 외 1명이 팔로우합니다",
+      "likeCount" : 2,
+      "content" : "핫한 카페라고 해서 와봄 넓어서 좋다",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "hyevly",
+          "comment" : "누구랑 갔냐"
+        }]
+    },
+    {
+      "id" : 10,
+      "userId" : "simple_diary",
+      "userName" : "David",
+      "intro" : "hyevley's blog / live in KR",
+      "profile" : "profilePic/profile9.jpg",
+      "pic" : "images/img9.jpg",
+      "overlap" : "chuibbo님 외 1명이 팔로우합니다",
+      "likeCount" : 3,
+      "content" : "파티파티💖 풍선 불다가 죽는줄^^... 그래도 다 하고 나니까 예뻐서 만족스러웠는데 사진을 못찍었네",
+      "comments" : [{
+          "id" : 1,
+          "유저id" : "yunE2",
+          "comment" : "꺄 ㅋㅋㅋㅋ 넘 뿌듯"
+        }]
+    }
+  ]
